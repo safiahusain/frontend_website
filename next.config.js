f2cbfb1405caaedcab819b7ba28ab62620f446d5
Bilal Fareed <91183266+baleybaa@users.noreply.github.com>
2024-02-07 13:10:10 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,16 @@
 /** @type {import('next').NextConfig} */
 const runtimeCaching = require("next-pwa/cache");
-const withPWA = require('next-pwa');
-const { hostname } = new URL(`${process.env.NEXT_PUBLIC_BASE_URL}`);
-const nextConfig={
+const withPWA = require("next-pwa");
+// const { hostname } = new URL(`${process.env.NEXT_PUBLIC_BASE_URL}`);
+const { hostname } = new URL(`https://admin.conquerortrading.com/`);
+
+const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: [`${hostname}`]
+    domains: [`${hostname}`],
   },
-}
+};
 const pwa = process.env.NEXT_PWA_STATUS;
 const nextConfigWithPwa = withPWA({
   dest: "public",
@@ -16,7 +18,5 @@ const nextConfigWithPwa = withPWA({
   skipWaiting: true,
   runtimeCaching,
   disable: process.env.NODE_ENV === "development",
-}) (
-    nextConfig
-);
-module.exports = pwa==='1'?nextConfigWithPwa:nextConfig ;
+})(nextConfig);
+module.exports = pwa === "1" ? nextConfigWithPwa : nextConfig;
