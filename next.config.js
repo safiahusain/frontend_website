c1373d9215510888e13713e9a973e185082d8ce5
Bilal Fareed <91183266+baleybaa@users.noreply.github.com>
2024-02-07 13:12:46 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const runtimeCaching = require("next-pwa/cache");
 const withPWA = require("next-pwa");
 // const { hostname } = new URL(`${process.env.NEXT_PUBLIC_BASE_URL}`);
-const { hostname } = new URL(`https://admin.conquerortrading.com/`);
-
+const { hostname } = new URL("https://admin.conquerortrading.com/");
+console.log(hostname);
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
